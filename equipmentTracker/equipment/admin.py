f2493d3cd0ccb0b30544094d0843b6461317228c
wcqrwtqr <mohalbatati@hotmail.com>
2022-02-10 00:24:56 +0300

    inital play with db

---
+++ equipmentTracker/equipment/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import equipment_DB
+
+admin.site.register(equipment_DB)
---
+++ equipmentTracker/jobID/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class JobsDB(models.Model):
+    JOBID = models.CharField(max_length = 20 ,unique=True,blank=False)
+    client = models.CharField(max_length=255,blank=False)
+    field = models.CharField(max_length=255,blank=True, null=True)
+    well = models.CharField(max_length=255,blank=False, null=True)
+    country = models.CharField(max_length=255,blank=False,default='Iraq')
+    BU = models.CharField(max_length= 3, default='KIU', null=True, blank=True)
+    BL = models.CharField(max_length= 3, default='SWT', null=True, blank=True)
+    description = models.CharField(max_length= 800, blank=True, null=True)
+    isContract = models.BooleanField(default=True, null=True, blank=True)
+    startDate = models.DateField(null=True, blank=True)
+    endDate = models.DateField(blank=True,null=True)
+    image = models.ImageField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now_add=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+
+    # def get_absolute_url(self):
+    #     return reverse('jobs_detail',kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return '%s %s %s'% (self.JOBID, self.client, self.well)
+
+    class Meta:
+        ordering = ['client']
